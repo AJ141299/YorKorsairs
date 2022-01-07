9927240a970cfbf32bc6a59d5267caa003baa6f4
Abdullah <abdullah141299@gmail.com>
2022-01-07 02:22:33 +0000

    designed according to prototype

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      font: {
+        'calibri': 'Calibri',
+      },
+      colors: {
+        'brown-bg': '#EADCCD',
+        'brown-btn-bg': '#483D30',
+        'brown-btn-bg-light': '#615240',
+        'brown-nav': '#51381F',
+        'brown-hover': '#ad8761',
+        'brown-text': '#644729',
+      },
+      animation: {
+        'wheel-spin': 'spin 1.5s ease-out',
+      },
+    },
   },
   plugins: [],
 };
